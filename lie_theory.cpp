c37fc8c8f44600aa80738143a27c91d3bd142436
旦明迪 <danmingdi.buenos@bytedance.com>
2022-10-17 19:02:56 +0800

    fix: add some comment

--- lie_theory.cpp
+++ lie_theory.cpp
@@ -1,12 +1,13 @@
 #include <Eigen/Eigen>
 #include <iostream>
 
-#include "Eigen/src/Core/Matrix.h"
 #include "sophus/se3.hpp"
 
-Eigen::Vector3d cost_func(Eigen::Vector3d zi, Eigen::Vector3d pi, Eigen::Matrix3d R) { return zi - R * pi; }
+Eigen::Vector3d cost_func(const Eigen::Vector3d& zi, const Eigen::Vector3d& pi, const Eigen::Matrix3d& R) {
+  return zi - R * pi;
+}
 
-Eigen::Matrix3d J(Eigen::Vector3d pi, Eigen::Matrix3d R) { return -Sophus::SO3d::hat(R * pi); }
+Eigen::Matrix3d J(const Eigen::Vector3d& pi, const Eigen::Matrix3d& R) { return -Sophus::SO3d::hat(R * pi); }
 
 int main() {
   // Prepare data.
@@ -14,15 +15,13 @@ int main() {
   std::vector<Eigen::Vector3d> pi = {{1, 2, 3}, {6, 3, 5}};
 
   // Initialize data.
-  // clang-format off
   Eigen::Matrix3d R_tmp = Eigen::AngleAxisd(0.4, Eigen::Vector3d(0, 0, 1)).toRotationMatrix();
   std::cout << "Initial R:\n" << R_tmp << std::endl;
   Sophus::SO3d R(R_tmp);
-  // clang-format on
 
-  // iter 10 times.
+  // Iter 10 times.
   for (int iter = 0; iter < 10; iter++) {
-    auto R_matrix = R.matrix();
+    const Eigen::Matrix3d& R_matrix = R.matrix();
     // Gauss-Newton iteration method.
     Eigen::Matrix3d H = Eigen::Matrix3d::Zero();
     Eigen::Vector3d g = Eigen::Vector3d::Zero();
@@ -34,8 +33,9 @@ int main() {
     // H * delta_phi = g
     Eigen::Vector3d delta_phi = H.inverse() * g;
 
+    // Update R.
     R = Sophus::SO3d::exp(delta_phi) * Sophus::SO3d(R);
   }
 
-  std::cout << "optimized R:\n" << R.matrix() << std::endl;
+  std::cout << "Optimized R:\n" << R.matrix() << std::endl;
 }
